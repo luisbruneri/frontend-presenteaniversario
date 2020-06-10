b93a22151e617eb8dd2adb2e9c6fe99a43d0ad01
luisbruneri <luisgustavobruneri@gmail.com>
2020-06-10 02:08:52 +0000

    Versão 01 (frontend)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return <h1>ReactJS, Template!</h1>
+import api from './api';
+
+function App() 
+{
+  const [lista, setLista] = useState([]); 
+
+  useEffect(() => {
+        api.get('/presente').then((response) =>{
+            const itens = response.data;
+            setLista(itens);
+        })
+}, [])
+
+  return (
+      <table>
+          {
+              lista.map(item =>(
+                  <tr key = {item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.nome_produto}</td>
+                      <td>{item.valor}</td>
+                      <td>{item.destinatario}</td>
+                  </tr>
+              ))
+          }
+
+      </table>
+  );
 }
 
 export default App;
